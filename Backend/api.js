1e21614ef2e018131c330ccfbe8f343da316d662
Vincent <vincent.hache@ig2i.fr>
2015-12-11 11:16:41 +0100

    Création de quelque route de base

---
+++ Backend/api.js
@@ -0,0 +1,60 @@
+var express = require('express');
+
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var routes = require('./routes/index');
+var user = require('./routes/user');
+var event =require('./routes/event');
+var login = require('./routes/login');
+var news = require('./routes/news')
+
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+    app.use('/', routes);
+    app.use('/user', user);
+    app.use('/event', event);
+    app.use('/login', login);
+    app.use('/news', news);
+
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({ error: err });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
+
+
+module.exports = app;
+
+app.listen(8080);
