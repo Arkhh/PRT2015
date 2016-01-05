d7f60e9867c573cf2c93670d73d19ac033083d53
jordan dominik <jordan dominik>
2016-01-05 21:23:00 +0100

    debut fonctions bdd

--- Backend/Test/Test.js
+++ Backend/Test/Test.js
@@ -1,7 +1,11 @@
 var request = require("request");
+var neo4j = require('node-neo4j');
 
 var host = 'localhost';
  port = 7474;
+var type;
+
+db = new neo4j('http://localhost:7474');
 
 var httpUrlForTransaction = 'http://' + host + ':' + port + '/db/data/transaction/commit';
 
@@ -13,6 +17,28 @@ function runCypherQuery(query, params, callback) {
         function (err, res, body) {
             callback(err, body);
         })
+}
+
+function lireNoeud(){
+    runCypherQuery(
+        'MATCH (n:TypeDeJoueur)RETURN n',
+        function (err, resp) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(resp);
+            }
+        }
+    )
+}
+
+function lireTruc() {
+    db.cypherQuery("MATCH (n:TypeDeJoueur)RETURN n", function (err, result) {
+        if (err) throw err;
+
+        console.log(result.data); // delivers an array of query results
+        //console.log(result.columns); // delivers an array of names of objects getting returned
+    });
 }
 
 /*runCypherQuery(
@@ -29,5 +55,40 @@ function runCypherQuery(query, params, callback) {
     }
 )*/
 
+function lireTrucType(type) {
+    db.cypherQuery("MATCH (n:TypeDeJoueur) WHERE n.Type = '" + type + "' RETURN n", function(err, result) {
+        if (err) throw err;
+    if (result.data != '[]') {
+        console.log(result.data); // delivers an array of query results
+    }
+        else {
+        console.log("pas de type de ce genre");
+    }//console.log(result.columns); // delivers an array of names of objects getting returned
+    });
+}
 
-;
+function connexion(username,password){
+    db.cypherQuery("MATCH (n:Personne) WHERE n.username = '" + username + "' AND n.password = '" + password + "' RETURN id(n)", function (err, resp) {
+        if (err) throw err;
+        console.log(resp.data);
+        //console.log(JSON.stringify(resp, null, 2));
+    //if (resp.data = '[]') return 0;
+        //console.log(resp.data);
+    })
+}
+
+function inscription(){
+    db.cypherQuery("CREATE(n:Personne { mail: 'mail.test1@gmail.com', valid: '0', adresse: '14 rue bidon1', Naissance: '24/12/1995', simple : '0'," +
+        "ville : 'Bruay', double : '1', photo : '', password:'PassBidon1', titre: 'membre', nom: 'Bouya', prenom: 'Chaka', username: " +
+        "'userBidon1', description: 'description bidon1', sexe : 'H'}) RETURN n", function(err,resp){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(resp);
+    }
+})
+}
+
+//lireTrucType('Vole');
+//connexion('Vince','MafrajVince');
+//inscription();
--- Backend/Test/Test2.js
+++ Backend/Test/Test2.js
@@ -5,7 +5,7 @@ var neo4j = require('node-neo4j');
 db = new neo4j('http://localhost:7474');
 
 //Run raw cypher with params
-db.cypherQuery(
+/*db.cypherQuery(
     'CREATE (somebody:Company { name: {name}, from: {company}, age: {age} }) RETURN somebody',
     {
         name: 'Cercle Laique',
@@ -18,19 +18,25 @@ db.cypherQuery(
         console.log(result.data); // delivers an array of query results
         console.log(result.columns); // delivers an array of names of objects getting returned
     }
-)
+)*/
 
-db.readNode(2, function(err, node){
-    if (err) throw err;
+function lireNoeud(idNoeud){
+    db.readNode(idNoeud, function(err, node){
+        if (err) throw err;
+        //console.log(node.age);
 
-    console.log("Je m'appelle " + node.name + ", j'ai " + node.age +"ans et je travaille chez " + node.company);
-    //console.log(node.age);
+        console.log(node._id);
+    })
+}
 
-    console.log(node._id);
-})
+function connexion(user, password){
+    db.readNode(user, function(err,node){
+        if (err) throw err;
+        console.log(node._id);
+    })
+}
 
-
-db.updateNode(2, {name:'Tom', company:'TheMafraj', age:'21'}, function(err, node){
+/*db.updateNode(4, {Type:'Fond'}, function(err,node){//, company:'TheMafraj', age:'21'}, function(err, node){
     if(err) throw err;
 
     if(node == true) {
@@ -38,5 +44,9 @@ db.updateNode(2, {name:'Tom', company:'TheMafraj', age:'21'}, function(err, node
     } else {
         // node not found, hence not update
     }
-})
+})*/
+
+lireNoeud(0);
+lireNoeud(5);
+//connexion(Jojo, MafrajJo);
 ;
