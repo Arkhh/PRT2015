10ba17ca06c1fa235393a902fad98dbed70d4b9c
unknown <thomas.sarek@gmail.com>
2016-01-06 15:49:19 +0100

    bop DB

--- Backend/Test/Test.js
+++ Backend/Test/Test.js
@@ -9,16 +9,68 @@ db = new neo4j('http://localhost:7474');
 
 var httpUrlForTransaction = 'http://' + host + ':' + port + '/db/data/transaction/commit';
 
-function runCypherQuery(query, params, callback) {
-    request.post({
-            uri: httpUrlForTransaction,
-            json: {statements: [{statement: query, parameters: params}]}
-        },
-        function (err, res, body) {
-            callback(err, body);
-        })
+var URL_API_BASE = "http://178.62.87.171:7474/db/data/cypher";
+var METHOD = {method: "POST"};
+
+var AUTH =  { headers:
+                {
+                    'Authorization': 'Basic bmVvNGo6bWFmcmFqMjAxNQ==',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+};
+
+
+
+
+function testCallback(error, response, body) {
+    console.log(response.body);
+    if (!error && response.statusCode == 200) {
+        var info = JSON.parse(response.body);
+        console.log(info);
+    }
+    if(error){
+        console.log(error);
+
+
+    }
 }
 
+prepareQuery("MATCH (n:TypeDeJoueur)RETURN n",testCallback);
+
+
+
+function prepareQuery(query,callback) {
+
+
+    var options = {
+        url: URL_API_BASE,
+        method: METHOD.method,
+        headers: AUTH.headers,
+        query: query,
+        params: {query: query}
+    };
+
+    runCypherQuery(options,callback);
+
+}
+
+function runCypherQuery(options,callback) {
+    request(options,callback);
+}
+
+
+
+
+
+/*db.cypherQuery("MATCH (n:TypeDeJoueur)RETURN n", function (err, result) {
+ if (err) throw err;
+
+ console.log(result.data); // delivers an array of query results
+ //console.log(result.columns); // delivers an array of names of objects getting returned
+ }); */
+
+
+/*
 function lireNoeud(){
     runCypherQuery(
         'MATCH (n:TypeDeJoueur)RETURN n',
@@ -31,15 +83,8 @@ function lireNoeud(){
         }
     )
 }
+*/
 
-function lireTruc() {
-    db.cypherQuery("MATCH (n:TypeDeJoueur)RETURN n", function (err, result) {
-        if (err) throw err;
-
-        console.log(result.data); // delivers an array of query results
-        //console.log(result.columns); // delivers an array of names of objects getting returned
-    });
-}
 
 /*runCypherQuery(
     'CREATE (somebody:Person { name: {name}, from: {company}, age: {age} }) RETURN somebody', {
@@ -54,7 +99,7 @@ function lireTruc() {
         }
     }
 )*/
-
+/*
 function lireTrucType(type) {
     db.cypherQuery("MATCH (n:TypeDeJoueur) WHERE n.Type = '" + type + "' RETURN n", function(err, result) {
         if (err) throw err;
@@ -65,17 +110,9 @@ function lireTrucType(type) {
         console.log("pas de type de ce genre");
     }//console.log(result.columns); // delivers an array of names of objects getting returned
     });
-}
+}*/
 
-function connexion(username,password){
-    db.cypherQuery("MATCH (n:Personne) WHERE n.username = '" + username + "' AND n.password = '" + password + "' RETURN id(n)", function (err, resp) {
-        if (err) throw err;
-        console.log(resp.data);
-        //console.log(JSON.stringify(resp, null, 2));
-    //if (resp.data = '[]') return 0;
-        //console.log(resp.data);
-    })
-}
+
 
 /*function inscription(){
     db.cypherQuery("CREATE(n:Personne { mail: 'mail.test1@gmail.com', valid: '0', adresse: '14 rue bidon1', Naissance: '24/12/1995', simple : '0'," +
@@ -90,6 +127,7 @@ function connexion(username,password){
 }*/
 
 //fonction ne gérant pas la photo ni la gestion des doublons
+/*
 function inscription(mail, adresse, Naissance, simple, ville, double, password, nom, prenom, username, description, sexe){
     db.cypherQuery("CREATE(n:Personne { mail: '" + mail + "', valid: '0', adresse: '"+ adresse + "', Naissance: '" + Naissance +"', simple : '0'," +
         "ville : '"+ ville +"', double : '" + double +"', photo : '', password:'" + password + "', titre: 'membre', nom: '" + nom + "', prenom: '" + prenom + "', username: " +
@@ -103,7 +141,18 @@ function inscription(mail, adresse, Naissance, simple, ville, double, password, 
 }
 
 
-lireTrucType('Volee');
+lireTrucType('Volee');*/
 //connexion('Vince','MafrajVince');
 //inscription();
 //inscription('jesus.christ@eglise.com','69 rue du Paradis','25/12/0000','1','Paradis','1','Allelouya','Christ','Jesus','Jeje','Oh my Gob','H');
+/*
+ function connexion(username,password){
+ runCypherQuery(query,params,callback)
+ db.cypherQuery("MATCH (n:Personne) WHERE n.username = '" + username + "' AND n.password = '" + password + "' RETURN id(n)", function (err, resp) {
+ if (err) throw err;
+ console.log(resp.data);
+ //console.log(JSON.stringify(resp, null, 2));
+ //if (resp.data = '[]') return 0;
+ //console.log(resp.data);
+ })
+ }*/
