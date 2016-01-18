947ee869e71d199d620172d06d438e73c451b860
unknown <thomas.sarek@gmail.com>
2016-01-18 01:41:44 +0100

    mafraj

--- Frontend/app/scripts/controllers/matches.js
+++ Frontend/app/scripts/controllers/matches.js
@@ -2,81 +2,125 @@
  * Created by Thomas on 12/01/2016.
  */
 angular.module('BadminTown')
-    .controller('MatchCtrl', function (MatchAPI,UserAPI, $rootScope, $scope, $location, $filter,$cookies) {
+    .controller('MatchCtrl', function (MatchAPI,UserAPI, $rootScope, $scope, $location, $filter,$cookies,$uibModal) {
+
+        $scope.getMatchesById = function(){
+            MatchAPI.getMatchUser($scope.userInfos.id)
+                .then(function(data){
+                    angular.forEach(data, function(value) {
+                        value.datePast=Date.create(parseInt(value.date)).isPast();
+                        value.dateStr=Date.create(parseInt(value.date)).relative('fr');
+                        UserAPI.getUserPubInfos(value.idJ2)
+                            .then(function(dataUserInfo){
+                                value.nom=dataUserInfo.nom;
+                                value.prenom=dataUserInfo.prenom;
+                                $scope.matches.push(value);
+                                console.log($scope.matches);
+                            },function(err){
+
+                            });
+
+                    });
+                },function(err){
+
+                })
+            ;
+
+        };
+
+        init();
 
 
-        $scope.players=[];
+        $scope.animationsEnabled = true;
 
-        $scope.players.push({
-            id:'123',
-            nom:'bop',
-            prenom:'zamel'
-        });
-        $scope.players.push({
-            id:'5',
-            nom:'bop2',
-            prenom:'zamel2'
-        });
+        $scope.openEdit = function (size) {
 
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'views/modals/modalEditMatch.html',
+                controller: 'EditMatchCtrl',
+                size: size
+            });
 
-        $scope.histoMatches=[];
+            modalInstance.result.then(function (selectedItem) {
+                console.log(selectedItem);
+            }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        };
 
-        $scope.histoMatches.push({
-            idMatch:'5',
-            idJ1:'70',
-            idJ2:'68',
-            date:Date.create(1452271844000).relative('fr'),
-            resultatJ1:'70',
-            resultatJ2:'68',
-            resultat:'',
-            noteJ1:'false',
-            noteJ2:'false',
-            gainJ1:15,
-            gainJ2:10,
-            perteJ1:15,
-            perteJ2:10
-        });
-        $scope.histoMatches.push({
-            id:'6',
-            nom:'bop2',
-            prenom:'zamel2',
-            date: Date.create(1452271844000).relative('fr'),
-            resultat:'D',
-            note:'false',
-            recompense:-10
-        });
+        $scope.openResult = function (size) {
+
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'views/modals/modalEditResultat.html',
+                controller: 'resultMatchCtrl',
+                size: size
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                console.log(selectedItem);
+            }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        };
 
 
+        function init(){
+
+            $scope.items = ['item1', 'item2', 'item3'];
 
 
-        $scope.matches=[];
+            $scope.dateToday=Date.create('today');
+            $scope.players=[];
 
-        $scope.matches.push({
-            idMatch:'5',
-            idJ1:'70',
-            idJ2:'68',
-            date:Date.create(1469039504000).relative('fr'),
-            resultatJ1:'70',
-            resultatJ2:'68',
-            resultat:'',
-            noteJ1:'false',
-            noteJ2:'false',
-            gainJ1:15,
-            gainJ2:10,
-            perteJ1:15,
-            perteJ2:10
-        });
-        $scope.matches.push({
-            id:'6',
-            nom:'bop2',
-            prenom:'zamel2',
-            date: Date.create(1469039404000).relative('fr'),
-            resultat:'D',
-            note:'false',
-            recompense:-10
-        });
+            $scope.players.push({
+                id:'123',
+                nom:'bop',
+                prenom:'zamel'
+            });
+            $scope.players.push({
+                id:'5',
+                nom:'bop2',
+                prenom:'zamel2'
+            });
 
 
+            $scope.histoMatches=[];
+
+            $scope.histoMatches.push({
+                idMatch:'5',
+                idJ1:'70',
+                idJ2:'68',
+                date:Date.create(1452271844000).relative('fr'),
+                resultatJ1:'70',
+                resultatJ2:'68',
+                resultat:'',
+                noteJ1:'false',
+                noteJ2:'false',
+                gainJ1:15,
+                gainJ2:10,
+                perteJ1:15,
+                perteJ2:10
+            });
+
+            $scope.histoMatches.push({
+                id:'6',
+                nom:'bop2',
+                prenom:'zamel2',
+                date: Date.create(1452271844000).relative('fr'),
+                resultat:'D',
+                note:'false',
+                recompense:-10
+            });
+
+            $scope.opponentCreate={id:'',nom:'',prenom:''};
+            $scope.opponentDisplay='';
+            $scope.matches=[];
+
+            $scope.getMatchesById();
+
+        }
 
         $scope.processSearch= function(simpleSearchText){
             if($scope.searchProcessing===true){
@@ -107,19 +151,14 @@ angular.module('BadminTown')
                 })
         };
 
-        $scope.opponentCreate={id:'',nom:'',prenom:''};
-        $scope.opponentDisplay='';
-
         $scope.getInfos = function () {
             MatchAPI.getUserInfo($scope.userInfos.id)
                 .then(function (data) {
                     if (!data.error) {
-
                         //$cookies.remove('userInfos');
                         //$cookies.putObject('userInfos',$scope.userinfos);
                     }
                 });
-
         };
 
         $scope.close = function(id){
@@ -150,4 +189,25 @@ angular.module('BadminTown')
             $scope.opponentDisplay=$scope.opponentCreate.nom+' '+$scope.opponentCreate.prenom
         };
 
-    });
+        $scope.createMatch = function(){
+            MatchAPI.createMatch()
+                .then(function(data){
+
+                },function(err){
+
+                });
+        };
+
+        $scope.deleteMatch = function(id){
+            MatchAPI.deleteMatch(id)
+                .then(function(){
+                    $scope.getMatchesById()
+
+                },function(err){
+
+                });
+        }
+
+
+
+});
